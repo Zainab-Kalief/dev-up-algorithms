241cc4bc1b83cf825a8c3dca6a3369b7524e3a2c
Zainab-Kalief <alesewura@yahoo.com>
2020-03-16 09:07:45 +0100

    array algo file added

---
+++ array-algorithms.js
@@ -0,0 +1,7 @@
+// CLASS 1 [14/03/2020]
+
+// 1
+// Given an array, a value [could be a number or a goat] and an index. Insert the given value at the particular index of the array
+// Arguments -> list = [10, 20, 40, 50, 60, 70], value = 30, index = 2
+// Expected result -> [10, 20, 30, 40, 50, 60, 70]
+// Order of complexity ?
